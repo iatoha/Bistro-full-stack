d9c930ea5e59079df53978289487a11081af1935
iatoha <iatohaph@gmail.com>
2024-11-04 23:58:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>This is a try Projects</h1>
+      <button class="btn btn-outline">Default</button>
+      <button class="btn btn-outline btn-primary">Primary</button>
+      <button class="btn btn-outline btn-secondary">Secondary</button>
+      <button class="btn btn-outline btn-accent">Accent</button>
+    </>
+  );
+}
+
+export default App;
