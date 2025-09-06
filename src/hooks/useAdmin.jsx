d9e89af6646b4c9c0ebbac13dce77d9e559e3a77
iatoha <iatohaph@gmail.com>
2025-09-06 12:21:08 +0600

    admin and user related

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -3,17 +3,18 @@ import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 
 const useAdmin = () => {
-    const  {user} = useAuth();
-    const axiosSecure = useAxiosSecure();
-    const {data: isAdmin} =  useQuery({
-        queryKey: [user?.email, 'isAdmin'],
-        queryFn: async() => {
-            const  res = await axiosSecure.get(`/users/admin/${user.email}`);
-            console.log(res.data);
-            return res.data?.admin;
-        }
-    })
-    return [isAdmin]
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+    queryKey: [user?.email, "isAdmin"],
+    enabled: !!user?.email, // only run when user exists
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/admin/${user.email}`);
+      console.log(res.data);
+      return res.data?.admin;
+    },
+  });
+  return [isAdmin, isAdminLoading];
 };
 
 export default useAdmin;
