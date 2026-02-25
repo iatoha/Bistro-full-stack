7f20486738e44602b81f3d6d1539e2113850c0b5
iatoha <iatohaph@gmail.com>
2026-02-25 19:31:11 +0600

    fix case-sensitive import error

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,7 +1,7 @@
 import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle";
 import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "./checkoutForm";
+import CheckoutForm from "./CheckoutForm";
 
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
